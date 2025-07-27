043bb3878fce85bab1b5967d1186ef05cb1bb133
jiseopshin <wltjq1203@icloud.com>
2025-07-28 00:07:32 +0900

    feat : 위치와 두 날짜가 주어지면 그 사이에 해당하는 기상정보 WeatherInfo를 반환하는 메서드 findByLocationAndDateBetween() 작성

--- src/main/java/com/team04/back/domain/weather/weather/repository/WeatherRepository.java
+++ src/main/java/com/team04/back/domain/weather/weather/repository/WeatherRepository.java
@@ -4,6 +4,10 @@ import com.team04.back.domain.weather.weather.entity.WeatherInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface WeatherRepository extends JpaRepository<WeatherInfo, Integer> {
+    List<WeatherInfo> findByLocationAndDateBetween(String location, LocalDate start, LocalDate end);
 }
