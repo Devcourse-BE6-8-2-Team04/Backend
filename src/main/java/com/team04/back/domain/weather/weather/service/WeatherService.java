1d02ad097bc0edef898af4bcaee15bbd3268faf9
jiseopshin <wltjq1203@icloud.com>
2025-07-28 00:20:02 +0900

    feat : 위치와 두 날짜가 주어지면 그 사이에 해당하는 기상정보 WeatherInfo를 반환하는 메서드 getDurationWeather() 작성

--- src/main/java/com/team04/back/domain/weather/weather/service/WeatherService.java
+++ src/main/java/com/team04/back/domain/weather/weather/service/WeatherService.java
@@ -1,7 +1,19 @@
 package com.team04.back.domain.weather.weather.service;
 
+import com.team04.back.domain.weather.weather.entity.WeatherInfo;
+import com.team04.back.domain.weather.weather.repository.WeatherRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class WeatherService {
+    private final WeatherRepository weatherRepository;
+
+    public List<WeatherInfo> getDurationWeather(String location, LocalDate start, LocalDate end) {
+        return weatherRepository.findByLocationAndDateBetween(location, start, end);
+    }
 }
