e2ce5fcf09b6e52150d41454ac906ef43024146f
jiseopshin <wltjq1203@icloud.com>
2025-07-28 11:34:59 +0900

    fix : getOutfitWithPeriod 가 ExtraCloth 를 포함할 수 있도록 수정

--- src/main/java/com/team04/back/domain/cloth/cloth/service/ClothService.java
+++ src/main/java/com/team04/back/domain/cloth/cloth/service/ClothService.java
@@ -1,26 +1,27 @@
 package com.team04.back.domain.cloth.cloth.service;
 
+import com.team04.back.domain.cloth.cloth.entity.Clothing;
 import com.team04.back.domain.cloth.cloth.entity.ClothInfo;
+import com.team04.back.domain.cloth.cloth.entity.ExtraCloth;
 import com.team04.back.domain.cloth.cloth.enums.Category;
 import com.team04.back.domain.cloth.cloth.repository.ClothRepository;
+import com.team04.back.domain.cloth.cloth.repository.ExtraClothRepository;
 import com.team04.back.domain.weather.weather.entity.WeatherInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList; // ArrayList import 추가
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ClothService {
     private final ClothRepository clothRepository;
+    private final ExtraClothRepository extraClothRepository;
 
-    public Map<Category, List<ClothInfo>> getOutfitWithPeriod(List<WeatherInfo> weatherPlan) {
-        Map<Category, List<ClothInfo>> recommendedClothesMap = new HashMap<>();
+    public Map<Category, List<Clothing>> getOutfitWithPeriod(List<WeatherInfo> weatherPlan) {
+        Map<Category, List<Clothing>> recommendedClothesMap = new HashMap<>();
 
         for (WeatherInfo weather : weatherPlan) {
             Double feelsLike = weather.getFeelsLikeTemperature();
@@ -29,10 +30,23 @@ public class ClothService {
 
             for (ClothInfo cloth : recommendedClothes) {
                 recommendedClothesMap
-                    .computeIfAbsent(cloth.getCategory(), k -> new ArrayList<>())
-                    .add(cloth);
+                        .computeIfAbsent(cloth.getCategory(), k -> new ArrayList<>())
+                        .add(cloth);
             }
+
+            getExtraClothes(weather)
+                    .forEach(extraCloth -> {
+                        recommendedClothesMap
+                                .computeIfAbsent(Category.EXTRA, k -> new ArrayList<>())
+                                .add(extraCloth);
+                    });
         }
         return recommendedClothesMap;
     }
+
+    private Set<ExtraCloth> getExtraClothes(WeatherInfo weather) {
+        Set<ExtraCloth> extraCloths = extraClothRepository.findDistinctByWeather(weather.getWeather());
+        return extraCloths;
+    }
+
 }
