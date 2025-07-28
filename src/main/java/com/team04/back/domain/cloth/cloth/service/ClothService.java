f068c7ec456d8ac9177195b66d80bfb00f4cb9cf
jiseopshin <wltjq1203@icloud.com>
2025-07-28 10:00:55 +0900

    feat : WeaterInfo 리스트가 주어지면 해당 기간에 적절한 의류의 종류와 벌수를 반환하는 메서드 getOutfitWithPeriod() 작성

--- src/main/java/com/team04/back/domain/cloth/cloth/service/ClothService.java
+++ src/main/java/com/team04/back/domain/cloth/cloth/service/ClothService.java
@@ -1,7 +1,38 @@
 package com.team04.back.domain.cloth.cloth.service;
 
+import com.team04.back.domain.cloth.cloth.entity.ClothInfo;
+import com.team04.back.domain.cloth.cloth.enums.Category;
+import com.team04.back.domain.cloth.cloth.repository.ClothRepository;
+import com.team04.back.domain.weather.weather.entity.WeatherInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList; // ArrayList import 추가
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ClothService {
+    private final ClothRepository clothRepository;
+
+    public Map<Category, List<ClothInfo>> getOutfitWithPeriod(List<WeatherInfo> weatherPlan) {
+        Map<Category, List<ClothInfo>> recommendedClothesMap = new HashMap<>();
+
+        for (WeatherInfo weather : weatherPlan) {
+            Double feelsLike = weather.getFeelsLikeTemperature();
+
+            List<ClothInfo> recommendedClothes = clothRepository.findByTemperature(feelsLike);
+
+            for (ClothInfo cloth : recommendedClothes) {
+                recommendedClothesMap
+                    .computeIfAbsent(cloth.getCategory(), k -> new ArrayList<>())
+                    .add(cloth);
+            }
+        }
+        return recommendedClothesMap;
+    }
 }
