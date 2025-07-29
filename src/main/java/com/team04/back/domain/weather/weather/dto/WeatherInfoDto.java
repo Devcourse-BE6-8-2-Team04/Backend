2c76f7ecb2e1c98521f49fa4539582964280cfa9
eunchan96 <taegwon02@gmail.com>
2025-07-29 09:36:30 +0900

    WeatherInfoDto 반영

--- src/main/java/com/team04/back/domain/weather/weather/dto/WeatherInfoDto.java
+++ src/main/java/com/team04/back/domain/weather/weather/dto/WeatherInfoDto.java
@@ -1,27 +1,44 @@
 package com.team04.back.domain.weather.weather.dto;
 
 import com.team04.back.domain.weather.weather.entity.WeatherInfo;
-import com.team04.back.domain.weather.weather.enums.Weather;
-import org.springframework.lang.NonNull;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record WeatherInfoDto(
-        @NonNull Weather weather,
-        @NonNull Double dailyTemperatureGap,
-        @NonNull Double feelsLikeTemperature,
-        @NonNull Double maxTemperature,
-        @NonNull Double minTemperature,
-        @NonNull String location,
-        @NonNull LocalDate date
-){
+        @NotNull int id,
+        @NotNull String weather,
+        String description,
+        @NotNull Double dailyTemperatureGap,
+        @NotNull Double feelsLikeTemperature,
+        @NotNull Double maxTemperature,
+        @NotNull Double minTemperature,
+        Double pop,
+        Double rain,
+        Double snow,
+        Integer humidity,
+        Double windSpeed,
+        Integer windDeg,
+        Double uvi,
+        @NotNull String location,
+        @NotNull LocalDate date
+) {
     public WeatherInfoDto(WeatherInfo weatherInfo) {
-        this(
-                weatherInfo.getWeather(),
+        this (
+                weatherInfo.getId(),
+                weatherInfo.getWeather().name(),
+                weatherInfo.getDescription(),
                 weatherInfo.getDailyTemperatureGap(),
                 weatherInfo.getFeelsLikeTemperature(),
                 weatherInfo.getMaxTemperature(),
                 weatherInfo.getMinTemperature(),
+                weatherInfo.getPop(),
+                weatherInfo.getRain(),
+                weatherInfo.getSnow(),
+                weatherInfo.getHumidity(),
+                weatherInfo.getWindSpeed(),
+                weatherInfo.getWindDeg(),
+                weatherInfo.getUvi(),
                 weatherInfo.getLocation(),
                 weatherInfo.getDate()
         );
