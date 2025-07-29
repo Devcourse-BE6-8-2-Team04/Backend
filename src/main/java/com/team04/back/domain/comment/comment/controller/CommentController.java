6fac71d1be28db45c4ba9e5a0d82c6f29b67b182
eunchan96 <taegwon02@gmail.com>
2025-07-29 09:15:25 +0900

    Dto 변환 다시 컨트롤러 단으로

--- src/main/java/com/team04/back/domain/comment/comment/controller/CommentController.java
+++ src/main/java/com/team04/back/domain/comment/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.team04.back.domain.comment.comment.controller;
 
+import com.team04.back.domain.comment.comment.entity.Comment;
 import com.team04.back.domain.comment.comment.dto.CommentDto;
 import com.team04.back.domain.comment.comment.service.CommentService;
 import com.team04.back.global.exception.ServiceException;
@@ -33,7 +34,7 @@ public class CommentController {
             throw new ServiceException("400-1", "location 파라미터 없이는 date 또는 feelsLikeTemperature 파라미터를 사용할 수 없습니다.");
         }
 
-        Page<CommentDto> items;
+        Page<Comment> items;
 
         if (location != null && date != null) {
             items = commentService.findByLocationAndDate(location, date, pageable);
@@ -43,6 +44,6 @@ public class CommentController {
             items = commentService.findAll(pageable);
         }
 
-        return items;
+        return items.map(CommentDto::new);
     }
 }
--- src/main/java/com/team04/back/domain/comment/comment/service/CommentService.java
+++ src/main/java/com/team04/back/domain/comment/comment/service/CommentService.java
@@ -1,11 +1,8 @@
 package com.team04.back.domain.comment.comment.service;
 
-import com.team04.back.domain.comment.comment.dto.CommentDto;
 import com.team04.back.domain.comment.comment.entity.Comment;
 import com.team04.back.domain.comment.comment.repository.CommentRepository;
-import com.team04.back.domain.weather.weather.entity.WeatherInfo;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Hibernate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,36 +16,21 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     @Transactional(readOnly = true)
-    public Page<CommentDto> findAll(Pageable pageable) {
-        return commentRepository.findAll(pageable)
-                .map(comment -> {
-                    WeatherInfo info = comment.getWeatherInfo();
-                    Hibernate.initialize(info);
-                    return new CommentDto(comment);
-                });
+    public Page<Comment> findAll(Pageable pageable) {
+        return commentRepository.findAll(pageable);
     }
 
     @Transactional(readOnly = true)
-    public Page<CommentDto> findByLocationAndDate(String location, LocalDate date, Pageable pageable) {
+    public Page<Comment> findByLocationAndDate(String location, LocalDate date, Pageable pageable) {
         int month = date.getMonthValue();
-        return commentRepository.findByWeatherInfoLocationAndMonth(location, month, pageable)
-                .map(comment -> {
-                    WeatherInfo info = comment.getWeatherInfo();
-                    Hibernate.initialize(info);
-                    return new CommentDto(comment);
-                });
+        return commentRepository.findByWeatherInfoLocationAndMonth(location, month, pageable);
     }
 
     @Transactional(readOnly = true)
-    public Page<CommentDto> findByLocationAndTemperature(String location, Double feelsLikeTemperature, Pageable pageable) {
+    public Page<Comment> findByLocationAndTemperature(String location, Double feelsLikeTemperature, Pageable pageable) {
         double minTemperature = feelsLikeTemperature - 2.5;
         double maxTemperature = feelsLikeTemperature + 2.5;
-        return commentRepository.findByWeatherInfoLocationAndFeelsLikeTemperature(location, minTemperature, maxTemperature, pageable)
-                .map(comment -> {
-                    WeatherInfo info = comment.getWeatherInfo();
-                    Hibernate.initialize(info);
-                    return new CommentDto(comment);
-                });
+        return commentRepository.findByWeatherInfoLocationAndFeelsLikeTemperature(location, minTemperature, maxTemperature, pageable);
     }
 
     public void save(Comment comment) {
